a0f665990f8474509af71d0ae23ac7694d797610
ysakamoto <yus.sakamoto@gmail.com>
2015-05-18 03:15:31 -0500

    time-series analysis

--- RE/time_analysis.py
+++ RE/time_analysis.py
@@ -9,12 +9,51 @@
 
 import numpy as np
 import pandas as pd
+import pylab as P
+
+bids_bots = pd.read_csv('data/bids_bots.csv')
+info_bots = pd.read_csv('data/info_bots.csv', index_col=0)
+
+bids_humans = pd.read_csv('data/bids_humans.csv')
+info_humans = pd.read_csv('data/info_humans.csv', index_col=0)
+
+min_interval = 52631578
+
+def plot_intervals_hist(bids, info, bidder_id):
+
+    bidstime = bids[bids['bidder_id']==bidder_id]['time'].values
+    bids_intervals = np.diff(bidstime)
+    bids_intervals = np.array(sorted(bids_intervals))/min_interval
+
+    bi_max = np.max(bids_intervals)
+
+    max_idx = sum(np.array(bids_intervals)<100)
+
+    # max_idx = 200
+    n, bins, patches = P.hist(bids_intervals[:max_idx], 50,
+                              histtype='stepfilled')
+    # P.show()
+
+    return bids_intervals
 
 
-# bids_bots = pd.read_csv('data/bids_bots.csv')
-# info_bots = pd.read_csv('data/info_bots.csv', index_col=0)
+for i in range(len(info_bots)):
+    try:
+        bi = plot_intervals_hist(bids_bots, info_bots, info_bots.index[i])
+        print bi[:100]
+    except:
+        print "error", i
+        pass
+    
+for i in range(len(info_humans)):
+    if (info_humans.iloc[i].num_bids)>200:
+        bi = plot_intervals_hist(bids_humans, info_humans, info_humans.index[i])
+        print bi[:100]
 
-# bidstime = bids_bots[bids_bots['bidder_id']==info_bots.index[1]]['time'].values
+
+print plot_intervals_hist(bids_humans, info_humans, info_humans.index[1872])
+
+sdf
 # bidstime -= bidstime[0]
 # bidstime /= 1e10
 
@@ -22,53 +61,20 @@ import pandas as pd
 
 
 bids_humans = pd.read_csv('data/bids_humans.csv')
-bids_bots = pd.read_csv('data/bids_bots.csv')
-
 info_humans = pd.read_csv('data/info_humans.csv', index_col=0)
-info_bots = pd.read_csv('data/info_bots.csv', index_col=0)
 
-ih = info_humans[info_humans.num_bids>100]
-for idx in ih.index:
+for idx in info_humans.index:
     print idx
     bh = bids_humans[bids_humans['bidder_id']==idx]
-    bidstime = bh['time'].values
-    bidstime -= bidstime[0]
+    bh['time'].values
     bidstime /= 1e10
 
     if len(bidstime)>100:
         print bidstime
 
-
-ib = info_bots[info_bots.num_bids>100]
-for idx in ib.index:
-    print idx
-    bh = bids_bots[bids_bots['bidder_id']==idx]
-    bidstime = bh['time'].values
-    bidstime -= bidstime[0]
-    bidstime /= 1e10
-
-    if len(bidstime)>100:
-        print bidstime
-
-
-
-# numb = pd.DataFrame(
-#     {'bots': info_bots.num_bids.values,
-#      'humans': info_humans.num_bids.values}, columns=['bots', 'humans'])
-
-# numb.plot(kind='hist')
-
-
-
-
-# plot distributions of num_*
-
-import scipy.stats
-
-_, ld = scipy.stats.boxcox(info_bots.num_ips)
-pd.DataFrame(scipy.stats.boxcox(info_bots.num_ips, ld)).plot(kind='hist', alpha=0.5)
-pd.DataFrame(scipy.stats.boxcox(info_humans.num_ips, ld)).plot(kind='hist', alpha=0.5)
-pd.DataFrame(scipy.stats.boxcox(info_test.num_ips, ld)).plot(kind='hist', alpha=0.5)
-plt.show()
+# extract the "bid frequency" data
+# objective: the interval between one bid to the other and generate a distribution.
+# create a bin and categorize them into ~10 intervals?
+# idea: bots should have more bids with small intervals than humans.
 
 
